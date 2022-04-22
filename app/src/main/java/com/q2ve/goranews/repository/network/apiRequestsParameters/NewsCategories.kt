53568c2fa03fb6e2d1028669e30f91993f14c8e2
q2ve <qwq2eq@gmail.com>
2022-04-22 17:39:02 +0300

    +|Network| Added NewsCategories.kt that represents possible categories for request to the server.

--- app/src/main/java/com/q2ve/goranews/repository/network/apiRequestsParameters/NewsCategories.kt
+++ app/src/main/java/com/q2ve/goranews/repository/network/apiRequestsParameters/NewsCategories.kt
@@ -7,4 +7,13 @@
 package com.q2ve.goranews.repository.network.apiRequestsParameters
 
 enum class NewsCategories {
+	Science,
+	Entertainment,
+	Sports,
+	General,
+	Health,
+	Technology,
+	Business;
+	
+	fun getCategoryName(): String = this.name.lowercase()
 }
