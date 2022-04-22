b7e52a30ee962bf6e1c230ad0b85dfe1c8d70d45
q2ve <qwq2eq@gmail.com>
2022-04-23 00:22:31 +0300

    +|Database| Added indexing methods with IndexItem.kt. +|Project| Added rxjava and kotlin-reflect implementations.

--- app/src/main/java/com/q2ve/goranews/MainActivity.kt
+++ app/src/main/java/com/q2ve/goranews/MainActivity.kt
@@ -7,11 +7,53 @@
 package com.q2ve.goranews
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.q2ve.goranews.repository.database.realm.RealmIO
+import com.q2ve.goranews.repository.database.realm.dataclasses.RealmItemArticle
+import com.q2ve.goranews.repository.network.apiRequestsParameters.NewsGettingParameters
+import com.q2ve.goranews.repository.network.retrofit.RetrofitCalls
+import io.realm.Realm
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		Realm.init(this)
+	}
+	
+	override fun onResume() {
+		super.onResume()
+		
+		val test = NewsGettingParameters(
+			"ac0793eb091f4bde9622dae5dfad2202",
+			"business",
+			8
+		)
+		RetrofitCalls().getNews(
+			test,
+			{
+				Log.e("Connection success", "logging articles....")
+				RealmIO().insertOrUpdateWithIndexing<RealmItemArticle>("DICK", it, true)
+				testLogArticles(it)
+			},
+			{
+				Log.e("Connection failed!", it.toString())
+				RealmIO().copyIndexedFromRealm<RealmItemArticle>("DICK",
+					{ tst ->
+						Log.e("Showing from database", "logging articles....")
+						testLogArticles(tst)
+					}
+				)
+			}
+		)
+	}
+	
+	private fun testLogArticles(list: List<RealmItemArticle>) {
+		Log.e("Articles", "----------------------------")
+		list.forEach {
+			Log.e("Article", it.title ?: "Название - null")
+		}
+		Log.e("/Articles", "----------------------------")
 	}
 }
--- app/src/main/java/com/q2ve/goranews/repository/database/realm/RealmIO.kt
+++ app/src/main/java/com/q2ve/goranews/repository/database/realm/RealmIO.kt
@@ -8,19 +8,26 @@ package com.q2ve.goranews.repository.database.realm
 
 import android.util.Log
 import com.q2ve.goranews.helpers.Constants
+import com.q2ve.goranews.repository.database.realm.dataclasses.IndexItem
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import io.realm.RealmObject
+import io.realm.Sort
+import kotlin.reflect.KMutableProperty
+import kotlin.reflect.KProperty
 
 class RealmIO {
 	val config: RealmConfiguration = RealmConfiguration.Builder()
 		.name(Constants.realmName)
 		.build()
 	
+	/**
+	 * Inserts or updates object in the database.
+	 */
 	fun <T: RealmObject> insertOrUpdate(
 		inputObject: T,
 		onSuccess: ((T) -> Unit)? = null,
-		onError: (() -> Unit)?
+		onError: (() -> Unit)? = null
 	) {
 		var output: T? = null
 		val realm = Realm.getInstance(config)
@@ -39,10 +46,13 @@ class RealmIO {
 		realm.close()
 	}
 	
+	/**
+	 * Inserts or updates objects in the database.
+	 */
 	fun <T: RealmObject> insertOrUpdate(
 		inputObjects: List<T>,
 		onSuccess: ((List<T>) -> Unit)? = null,
-		onError: (() -> Unit)?
+		onError: (() -> Unit)? = null
 	) {
 		var output: List<T> = emptyList()
 		val realm = Realm.getInstance(config)
@@ -60,10 +70,13 @@ class RealmIO {
 		realm.close()
 	}
 	
+	/**
+	 * Copies object from the database by it's primary key.
+	 */
 	inline fun <reified T: RealmObject> copyFromRealm(
 		id: String,
 		noinline onSuccess: ((T) -> Unit),
-		noinline onError: (() -> Unit)?
+		noinline onError: (() -> Unit)? = null
 	) {
 		var output: T? = null
 		val realm = Realm.getInstance(config)
@@ -83,9 +96,12 @@ class RealmIO {
 		realm.close()
 	}
 	
+	/**
+	 * Copies all objects of the provided class from the database.
+	 */
 	inline fun <reified T: RealmObject> copyFromRealm(
 		noinline onSuccess: ((List<T>) -> Unit),
-		noinline onError: (() -> Unit)?
+		noinline onError: (() -> Unit)? = null
 	) {
 		var output: List<T> = emptyList()
 		val realm = Realm.getInstance(config)
@@ -102,4 +118,163 @@ class RealmIO {
 		)
 		realm.close()
 	}
+	
+	/**
+	 * Deletes provided object from the database.
+	 */
+	fun <T: RealmObject> delete(
+		inputObject: T,
+		onSuccess: (() -> Unit)? = null,
+		onError: (() -> Unit)? = null
+	) {
+		val realm = Realm.getInstance(config)
+		realm.executeTransactionAsync(
+			{ r: Realm ->
+				val objectInRealm = r.copyToRealmOrUpdate(inputObject)
+				objectInRealm.deleteFromRealm()
+			}, { onSuccess?.let { it() } }, { t: Throwable ->
+				Log.e("RealmIO.delete single", t.toString())
+				onError?.invoke()
+			}
+		)
+		realm.close()
+	}
+	
+	/**
+	 * Deletes all objects of the provided class from the database.
+	 */
+	inline fun <reified T: RealmObject> delete(
+		noinline onSuccess: (() -> Unit)? = null,
+		noinline onError: (() -> Unit)? = null
+	) {
+		val realm = Realm.getInstance(config)
+		realm.executeTransactionAsync(
+			{ r: Realm ->
+				val objectsInRealm = r.where(T::class.java).findAll()
+				objectsInRealm.deleteAllFromRealm()
+			}, { onSuccess?.let { it() } }, { t: Throwable ->
+				Log.e("RealmIO.delete multi", t.toString())
+				onError?.invoke()
+			}
+		)
+		realm.close()
+	}
+	
+	/**
+	 * This function is needed to index items we got from the server
+	 * and then to adding them to the local database.
+	 */
+	inline fun <reified T: RealmObject> insertOrUpdateWithIndexing(
+		listName: String,
+		inputObjects: List<RealmObject>,
+		clearOldList: Boolean,
+		noinline onSuccess: (() -> Unit)? = null,
+		noinline onError: (() -> Unit)? = null
+	) {
+		val realm = Realm.getInstance(config)
+		realm.executeTransactionAsync(
+			{ r: Realm ->
+				if (clearOldList) {
+					val oldIndexItems = r.where(IndexItem::class.java)
+						.equalTo("listName", listName)
+						.findAll()
+					oldIndexItems.deleteAllFromRealm()
+				}
+				
+				var lastIndex = 0
+				r.where(IndexItem::class.java)
+					.equalTo("listName", listName)
+					.findAll()
+					.forEach { if (it.index > lastIndex) lastIndex = it.index }
+				inputObjects.forEach { realmObject ->
+					r.copyToRealmOrUpdate(realmObject)
+					/**
+					 * Index items needs to get the objects from DB
+					 * in the correct order they came from the server.
+					 */
+					val indexItem = IndexItem()
+					indexItem.index = lastIndex + 1
+					indexItem.listName = listName
+					/**
+					 * A Great Reflexive Magic that finds right field in the index item
+					 * and fills it with the inputted object.
+					 * It's necessary for this code to work with objects of any classes
+					 * that i might add in the future.
+					 * But for every new realm dataclass, that we want to index, we must
+					 * add new field in the IndexItem.
+					 */
+					val fields = IndexItem::class.members
+					val field = fields.find {
+						it.returnType.classifier.toString() == T::class.java.toString()
+					}
+					if (field is KMutableProperty) {
+						field.setter.call(indexItem, realmObject)
+					}
+					r.insertOrUpdate(indexItem)
+					lastIndex++
+				}
+			}, { onSuccess?.invoke() }, { t: Throwable ->
+				Log.e("RealmIO.insertOrUpdateWithIndexing", t.toString())
+				onError?.invoke()
+			}
+		)
+		realm.close()
+	}
+	
+	/**
+	 * Retrieves items from database in the order they were put there.
+	 * Make sure your "T" matches class of the objects which stores
+	 * in the list with name you specified.
+	 */
+	inline fun <reified T: RealmObject> copyIndexedFromRealm(
+		listName: String,
+		noinline onSuccess: (List<T>) -> Unit,
+		noinline onError: (() -> Unit)? = null
+	) {
+		var output: List<T> = emptyList()
+		val realm = Realm.getInstance(config)
+		realm.executeTransactionAsync(
+			{ r: Realm ->
+				/**
+				 * Sorting objects from DB with index items (also from DB).
+				 */
+				val indexObjectsSortedList = r.where(IndexItem::class.java)
+					.equalTo("listName", listName)
+					.sort("index", Sort.ASCENDING)
+					.findAll()
+				val sortedList: MutableList<T> = emptyList<T>().toMutableList()
+				indexObjectsSortedList.forEach { indexItem ->
+					val fields = IndexItem::class.members
+					val field = fields.find {
+						it.returnType.classifier.toString() == T::class.java.toString()
+					}
+					if (field is KProperty) {
+						sortedList += field.getter.call(indexItem) as T
+					}
+				}
+				/**
+				 * Copying objects from DB. Now they're not related to DB.
+				 */
+				output = r.copyFromRealm(sortedList)
+			}, { onSuccess.invoke(output) }, { t: Throwable ->
+				Log.e("RealmIO.copyIndexedFromRealm", t.toString())
+				onError?.invoke()
+			}
+		)
+		realm.close()
+	}
+	
+//	fun getFilteredNewsResults(
+//		category: NewsCategories,
+//		onSuccess: (ResultsSetNews) -> Unit,
+//		onError: (() -> Unit)? = null
+//	) {
+//		val realm = Realm.getInstance(config)
+//		val test = realm.where(RealmItemArticle::class.java).equalTo("category", category)
+//			.equalTo("isClosed", isClosed)
+//			.lessThan("endDate", limitDate)
+//			.sort("title", Sort.ASCENDING)
+//			.findAll()
+//		//return DeadlinesRealmResultsSet(list, realm)
+//	}
 }
