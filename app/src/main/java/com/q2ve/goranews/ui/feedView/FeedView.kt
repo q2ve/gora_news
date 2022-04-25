10dad9e6c54e9d97f3a2e921cb8b7cc2a52c9509
q2ve <qwq2eq@gmail.com>
2022-04-25 16:59:21 +0300

    *|Feed| Small fix 2.

--- app/src/main/java/com/q2ve/goranews/ui/feedView/FeedView.kt
+++ app/src/main/java/com/q2ve/goranews/ui/feedView/FeedView.kt
@@ -42,7 +42,7 @@ class FeedView: FeedViewInterface {
 		recycler.setHasFixedSize(true)
 		//recycler.setItemViewCacheSize(4)
 		
-		val preloader = adapter.buildPreloader(context, 8)
+		val preloader = adapter.buildPreloader(context, 12)
 		recycler.addOnScrollListener(preloader)
 		
 		//Dummy listener is necessary to restrict any interaction with recycler.
